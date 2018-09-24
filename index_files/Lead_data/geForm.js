30483cfa98b52b832260ddd1216ebb7299922bd5
Irisbaijie <42249939+Irisbaijie@users.noreply.github.com>
2018-09-24 14:04:58 -0400

    Add original files via upload

---
+++ index_files/Lead_data/geForm.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function() {
+    $("input.clf-a-date").each(function() {
+        $(this).datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "-50:+10"
+        });
+    })
+    var dialogWidth = $(window).width()*.75
+    if ($(window).width() < dialogWidth) {
+        dialogWidth = $(window).width() - 50;
+    }
+    
+    if ($("#FormProcessResult").html() != "") {
+        $("#FormProcessResultDiv").dialog({
+            modal: true,
+            width: dialogWidth
+        });
+    }
+
+    if ($("#ThankYouContent").html() != "") {
+        $("#ThankyouDiv").dialog({
+             modal: true,
+             width: dialogWidth,
+            close: function(event, ui) { if ($("#ThankyouDiv").attr("action") != "") window.location.href = $("#ThankyouDiv").attr("action"); }
+        });
+    }
+    else if ($("#ThankyouDiv").attr("action") != "") {
+        window.location.href = $("#ThankyouDiv").attr("action");
+    }
+
+    $("form input[maxlength]").on('input', function() {
+        if ($(this).val().length == $(this).attr('maxlength')) {
+            $(this).next("input").focus();
+        }
+    });
+
+    $.getScript('/JavaScript/jquery.placeholder.js', function() {
+        $('input, textarea').placeholder();
+    });
+    
+    var externalStyleUrl = GetQueryStringParams('Style');
+    if(externalStyleUrl){
+        $('#ExternalStyle').attr('href',decodeURIComponent(externalStyleUrl));
+    }
+});
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event)
+{
+
+  //var origin = event.origin || event.originalEvent.origin; // For Chrome, the origin property is in the event.originalEvent object.
+  //if (origin !== "http://allowedorigin.com") return;
+  
+  for (var item in event.data){
+        $(event.data[item][0]).val(event.data[item][1]);
+  }
+  
+
+}
+
+function GetQueryStringParams(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
---
+++ statics/js/js.js
@@ -0,0 +1,56 @@
+// main menu  
+function showTab01() {
+	if (document.getElementById) {
+		document.getElementById('tab01').style.display = 'block';
+		document.getElementById('tab02').style.display = 'none';
+		document.getElementById('tab03').style.display = 'none';
+		document.getElementById('linkTab01').style.background = '#e9e9e9';
+		document.getElementById('linkTab01').style.color = '#58585a';
+		document.getElementById('linkTab02').style.background = '#a9a9a9';
+		document.getElementById('linkTab02').style.color = '#e9e9e9';
+		document.getElementById('linkTab03').style.background = '#a9a9a9';
+		document.getElementById('linkTab03').style.color = '#e9e9e9';
+	}
+}
+function showTab02() {
+	if (document.getElementById) {
+		document.getElementById('tab01').style.display = 'none';
+		document.getElementById('tab02').style.display = 'block';
+		document.getElementById('tab03').style.display = 'none';
+		document.getElementById('linkTab02').style.background = '#e9e9e9';
+		document.getElementById('linkTab02').style.color = '#58585a';
+		document.getElementById('linkTab01').style.background = '#a9a9a9';
+		document.getElementById('linkTab01').style.color = '#e9e9e9';
+		document.getElementById('linkTab03').style.background = '#a9a9a9';
+		document.getElementById('linkTab03').style.color = '#e9e9e9';
+	}
+}
+function showTab03() {
+	if (document.getElementById) {
+		document.getElementById('tab01').style.display = 'none';
+		document.getElementById('tab02').style.display = 'none';
+		document.getElementById('tab03').style.display = 'block';
+		document.getElementById('linkTab03').style.background = '#e9e9e9';
+		document.getElementById('linkTab03').style.color = '#58585a';
+		document.getElementById('linkTab01').style.background = '#a9a9a9';
+		document.getElementById('linkTab01').style.color = '#e9e9e9';
+		document.getElementById('linkTab02').style.background = '#a9a9a9';
+		document.getElementById('linkTab02').style.color = '#e9e9e9';
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
